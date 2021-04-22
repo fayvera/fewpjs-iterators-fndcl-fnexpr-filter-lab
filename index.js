cbdc724ce34ffae7b1f7ba9eb3ab8b52feb1fbe2
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-22 15:34:07 -0400

    commit tree d8858ec1ed25ad371ede3a404e88577d4e38224f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter( n => 
+        n.toLowerCase() === name.toLowerCase() 
+    )
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter (n =>
+        n.charAt(0) === name.charAt(0)
+    )
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( n => 
+        n.name === string)
+}
+
